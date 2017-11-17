55b8ddd08ec8a92bd486947e5b8edb00540c035e
Kashmira9890 <kashmiranagwekar@gmail.com>
2017-11-17 14:12:13 +0530

    Ques. switch time added to livetime when only one socket is connected.

--- server.js
+++ server.js
@@ -26,8 +26,6 @@
 var app 		= require('express')();
 var http 		= require('http').createServer(app);
 var io			= require('socket.io').listen(http);
-var util 		= require('util');
-var CircularJSON	= require('circular-json');
 
 var bodyParser 	= require('body-parser');
 var mysql 		= require('mysql');
@@ -35,8 +33,8 @@ var mysql 		= require('mysql');
 var con = mysql.createConnection({
 	host : "localhost",
 	user : "root",
-	password : "root@mysql",
-	database : "trial"
+	password : "root123",
+	database : "trialdb"
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -49,164 +47,180 @@ var port = 3000;
 
 var record = io.sockets.on('connection', function (socket) {	
 	console.log( socket.id + ' connected');
-	
-  
-	
+		
 	socket.on('attempt', function(data) {
-	
-		    socket.username = "'" + data.username + "'";
-	        socket.quizid = data.quizid;
-	        socket.roomid = "'" + data.roomid + "'";
-	        socket.socketid = "'" + socket.id + "'";
-	        socket.statusConnected = "'Connected'";
-	        socket.timestampC = socket.handshake.issued;
-	        socket.ip = "'" + socket.request.connection.remoteAddress + "'";
-	        
-	     var socketobject = {  username : socket.username, 
-	    						quizid : socket.quizid, 
-	    						roomid : socket.roomid, 
-	    					  socketid : socket.id, 
-	    						    ip : socket.ip
-	    					};
+		// Append some extra data to the socket object.
+	    socket.username 	= "'" + data.username + "'";
+	    socket.quizid 		= data.quizid;
+        socket.roomid 		= "'" + data.roomid + "'";
+        socket.socketid 	= "'" + socket.id + "'";
+        socket.statusConnected = "'Connected'";
+        socket.timestampC 	= socket.handshake.issued;
+        socket.ip 			= "'" + socket.request.connection.remoteAddress + "'";
 
-	    // Insert connection record into database.
-	     var sql = "INSERT INTO socketinfo (username,quizid,roomid,socketid,socketstatus,ip,timestamp) VALUES" +
-         			"(" + socket.username + "," + socket.quizid + "," + socket.roomid + "," + socket.socketid + 
-         			"," + socket.statusConnected + "," + socket.ip + "," + socket.timestampC + ")";
-	     con.query(sql, function(err, result) {
-			   if (err) throw err;
-			});
+	    // Insert connection record into the database.
+        // 'socketinfo' table has records of all the socket connections and disconnections.
+	    var sql = "INSERT INTO socketinfo (username, quizid, roomid, socketid, socketstatus, ip, timestamp) VALUES" +
+         			"(" + socket.username + "," 
+         				+ socket.quizid + "," 
+         				+ socket.roomid + "," 
+         				+ socket.socketid + "," 
+         				+ socket.statusConnected + "," 
+         				+ socket.ip + "," 
+         				+ socket.timestampC + ")";
+	    con.query(sql, function(err, result) {
+	    	if (err) throw err;
+		});
 
-	    //Joining the connected SID to the ROOM
+	    // Join the connected socket to the room. 'roomid' is the concatenaton of (username + quizid).
+	    // Here, 'roomid' corresponds to a particular user.
 	    socket.join(socket.roomid);	   
-	    //Finding the total connected SIDs to the ROOM
-	    var totalconnectedsockets= io.sockets.adapter.rooms[socket.roomid].length;   
+	    
+	    // Find the total connected sockets in the room.
+	    var totalconnectedsockets = io.sockets.adapter.rooms[socket.roomid].length;   
 
 	    if (totalconnectedsockets > 0) {
-	    	socket.livestatus = "'Live'";
+	    	socket.currentstatus = "'Live'";
            
-	    	var liverecordexistsql = "Select * from livetable1 where roomid="+socket.roomid;
-     
+	    	// 'livetable1' reflects current status of a user. There is one entry per user in this table. 
+	    	// If exists, fetch previous entry for this user from 'livetable1'.
+	    	var liverecordexistsql = "SELECT * FROM livetable1 WHERE roomid = " + socket.roomid;
 	    	con.query(liverecordexistsql, function(err, result) {
                 if (err) throw err;
+                
                 if (result.length > 0) {
-
                     for (i in result) {
-                        status = result[i].status;
-                        timetoconsider = result[i].timetoconsider;
-                        var deadtime = result[i].deadtime;
-                        console.log(socket.roomid + ': Previous deadtime: ' + humanise(deadtime));
+                        var status 			= result[i].status;
+                        var timetoconsider 	= result[i].timetoconsider;
+                        var deadtime 		= result[i].deadtime;
+                        var livetime 		= result[i].livetime;
+                        console.log(socket.roomid + ': Previous deadtime: ' + humanise(deadtime) + ' Status: ' + status);
                     }
-
                     if (status == 'Dead') {
-                        console.log(socket.roomid + ': Current Deadtime is :' + humanise(socket.timestampC - timetoconsider));
-                        deadtime = parseInt(deadtime) + parseInt(socket.timestampC - timetoconsider);
-                        console.log(socket.roomid + ': After cumulation,deadtime is : ' + humanise(deadtime));
-
-                        var updatelivetablesql = "Update livetable1 set status=" + socket.livestatus + ",deadtime=" + deadtime +
-                                                 ",timetoconsider=" + socket.timestampC + " where roomid=" + socket.roomid;
-                        con.query(updatelivetablesql, function(err, result) {
-                            if (err) throw err;
-                        });
+                        console.log(socket.roomid + ': Current deadtime is: ' + humanise(socket.timestampC - timetoconsider));
+                        
+                        // Check whether it is a ques switch or not. Ques switch time is approx. betwn. 0-2 secs.
+                        // This is required when there is only one connected socket for that user.
+                        if((socket.timestampC - timetoconsider) > 2000) {
+	                        // Compute cumulative deadtime.
+	                        deadtime = parseInt(deadtime) + parseInt(socket.timestampC - timetoconsider);
+	                        console.log(socket.roomid + ': After cumulation, deadtime is: ' + humanise(deadtime));
+	
+	                        // Update 'status' entry for this user in 'livetable1'.
+	                        var updatelivetablesql = "UPDATE livetable1 SET status = " 	+ socket.currentstatus 
+	                        								+ ", deadtime = "  	+ deadtime 
+	                        								+ ", timetoconsider = " + socket.timestampC 
+	                        								+ " where roomid = " + socket.roomid;
+	                        con.query(updatelivetablesql, function(err, result) {
+	                            if (err) throw err;
+	                        });
+                        } else {	
+                        	// Add ques switch time to the live time.
+                        	// Compute cumulative livetime.
+            		    	livetime = parseInt(livetime) + parseInt(socket.timestampC - timetoconsider);
+            		    	console.log(socket.roomid + ': After cumulation, livetime  is: ' + humanise(livetime));
+            			
+            		    	// Update 'status' entry for this user in 'livetable1'.
+            		    	var updatelivetablesql = "UPDATE livetable1 SET status = " + socket.currentstatus 
+            		    									+ ", timetoconsider = " + socket.timestampC 
+            		    									+ ", livetime = " + livetime 
+            		    									+ " where roomid = " + socket.roomid;
+            			    con.query(updatelivetablesql, function(err, result) {
+            			    	if (err) throw err;
+            			    });
+                        }
                     }
-
                 } else {
-                    var livetablesql = "INSERT INTO livetable1 (roomid,status,timetoconsider,livetime,deadtime) VALUES" +
-                                      "(" + socket.roomid + "," + socket.livestatus + "," + socket.timestampC + ",0,0 )";
+                	// Insert current status entry for this user in 'livetable1'.                	
+                    var livetablesql = "INSERT INTO livetable1 (roomid, status, timetoconsider, livetime, deadtime) VALUES" +
+                                      	"(" + socket.roomid + "," 
+                                      		+ socket.currentstatus + "," 
+                                      		+ socket.timestampC + ", 0, 0 )";
                     con.query(livetablesql, function(err, result) {
                         if (err) throw err;
                     });
                 }
             });
-          }
-	    
-
+	    }
 	});
 	
 	
 	socket.on('disconnect', function() {
-		console.log( socket.id + ' disconnected');
+		console.log(socket.id + ' disconnected');
 				
-		// Construct record for socket disconnected.
+		// Construct record for the disconnected socket.
 		socket.timestampD = new Date().getTime();
 		socket.statusDisconnected = "'Disconnected'";
 	  	    
 	    // Insert disconnection record into database.
-		var sql = "INSERT INTO socketinfo (username,quizid,roomid,socketid,socketstatus,ip,timestamp) VALUES" +
-				  "(" + socket.username + "," + socket.quizid + "," + socket.roomid + "," + socket.socketid + 
-				  "," + socket.statusDisconnected + "," + socket.ip + "," + socket.timestampD + ")";
-
+		var sql = "INSERT INTO socketinfo (username, quizid, roomid, socketid, socketstatus, ip, timestamp) VALUES" +
+				  	"(" + socket.username + "," 
+				  		+ socket.quizid + "," 
+				  		+ socket.roomid + "," 
+				  		+ socket.socketid + "," 
+				  		+ socket.statusDisconnected + "," 
+				  		+ socket.ip + "," 
+				  		+ socket.timestampD + ")";
 	    con.query(sql, function(err, result) {
 	    	if (err) throw err;	  
 	    });
 	    
-	
-	    
-//	    //This is not providing the live connected sockets,as room is getting empty on disconnection,I guess.
-//	    So currently using db,to get the live status.
-//	    var clients = io.sockets.adapter.rooms[socket.roomid].sockets;   
-//	    
-//	    var numClients = (typeof clients !== 'undefined') ? Object.keys(clients).length : 0;
-//	    console.log('still there are '+ numClients+ ' connected sockets');
-	    
-	      var liveordead = "select username,quizid,roomid," +
-          "count(case when socketstatus = 'Connected' then socketid end) as count_connected_socketid, " +
-          "count(case when socketstatus = 'Disconnected' then socketid end) as count_disconnected_socketid " +
-          "from socketinfo where roomid=" + socket.roomid +
-          "group by username,quizid,roomid";
-      
-	      con.query(liveordead, function(err, result) {
-          if (err) throw err;
-          for (i in result) {
-              countC = result[i].count_connected_socketid;
-              countD = result[i].count_disconnected_socketid;
-          }
-
-          if (countC == countD) {
-              socket.livestatus = "'Dead'";
-
-              var fetchtimesql = "select timetoconsider,livetime from livetable1 where roomid=" + socket.roomid;
-
-              con.query(fetchtimesql, function(err, result) {
-                  if (err) throw err;
-
-                  for (i in result) {
-                      var timetoconsider = result[i].timetoconsider;
-                      var livetime = result[i].livetime;
-                      console.log(socket.roomid + ':Previous livetime: ' + humanise(livetime));
-                  }
-
-                  //  here socket.timestampD is the maxdisconnecttime;
-                  console.log(socket.roomid + ':Current livetime is :' + humanise(socket.timestampD - timetoconsider));
-                  livetime = parseInt(livetime) + parseInt(socket.timestampD - timetoconsider);
-                  console.log(socket.roomid + ':After cumulation,livetime  is :' + humanise(livetime));
-
-                  var updatelivetablesql = "Update livetable1 set status=" + socket.livestatus + ", timetoconsider=" +
-                      socket.timestampD + ",livetime=" + livetime + "  where roomid=" + socket.roomid;
-
-                  con.query(updatelivetablesql, function(err, result) {
-                      if (err) throw err;
-                  });
-              });
-          }
-      });
-  
-	});
-   
+	    // Find the total connected sockets in the room.
+        var rooms = io.sockets.adapter.rooms[socket.roomid]; 
+        var totalconnectedsockets;
+        if(typeof rooms != 'undefined') {
+  	    	totalconnectedsockets = io.sockets.adapter.rooms[socket.roomid].length;     	    	
+  	    } else {
+  	    	totalconnectedsockets = 0;
+  	    }
+//        console.log('totalconnectedsockets: ' + totalconnectedsockets);
+  	    
+  	    if(totalconnectedsockets == 0) {          
+		    socket.currentstatus = "'Dead'";
+		    
+		    // Fetch previous entry for this user from 'livetable1'.
+		    var fetchtimesql = "SELECT timetoconsider, livetime FROM livetable1 WHERE roomid = " + socket.roomid;
+		    con.query(fetchtimesql, function(err, result) {
+		    	if (err) throw err;
+		
+		    	for (i in result) {
+		    		var timetoconsider = result[i].timetoconsider;
+		    		var livetime = result[i].livetime;
+		    		console.log(socket.roomid + ': Previous livetime: ' + humanise(livetime));
+		    	}
+		
+		    	// Here, socket.timestampD is the maxdisconnecttime.
+		    	console.log(socket.roomid + ': Current livetime is: ' + humanise(socket.timestampD - timetoconsider));
+		    	
+		    	// Compute cumulative livetime.
+		    	livetime = parseInt(livetime) + parseInt(socket.timestampD - timetoconsider);
+		    	console.log(socket.roomid + ': After cumulation, livetime  is: ' + humanise(livetime));
+			
+		    	// Update 'status' entry for this user in 'livetable1'.
+		    	var updatelivetablesql = "UPDATE livetable1 SET status = " + socket.currentstatus 
+		    									+ ", timetoconsider = " + socket.timestampD 
+		    									+ ", livetime = " + livetime 
+		    									+ " where roomid = " + socket.roomid;
+			    con.query(updatelivetablesql, function(err, result) {
+			    	if (err) throw err;
+			    });
+			});
+		}
+	}); 
 });
 
 function humanise(difference) {
-    var days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    var time = days + ' days, ' + hours + ' hrs, ' + minutes + ' mins, ' + seconds + ' secs';
+    var days 	 = Math.floor(difference / (1000 * 60 * 60 * 24));
+    var hours 	 = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes  = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    var mseconds = Math.floor((difference % (1000 * 60)));
+    var time 	 = days + ' days, ' + hours + ' hrs, ' + minutes + ' mins, ' + mseconds + ' msecs';
+    
     return time;
 }
 
-
 app.get('/livestatus', function(req, res) {
-    var sql = "select * from livetable1 ";
+    var sql = "SELECT * FROM livetable1";
     con.query(sql, function(err, result, fields) {
         if (err) throw err;
         res.send(result);
